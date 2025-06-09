8a519f28d450786fcf603b31a840eb70f29d97a5
MaxwellZhang7 <maxwellzhg@gmail.com>
2025-06-09 09:46:21 +0800

    Update answer_search_law.py

--- answer_search_law.py
+++ answer_search_law.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 # coding: utf-8
-# File: answer_search.py
-# Author: lhy<lhy_in_blcu@126.com,https://huangyong.github.io>
-# Date: 18-10-5
+
 from flask import Flask, render_template
 from neo4j import GraphDatabase
 from py2neo import Graph
